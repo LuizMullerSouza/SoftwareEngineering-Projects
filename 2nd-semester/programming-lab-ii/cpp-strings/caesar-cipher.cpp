e572b555db6b3bc8b095476f6573b8690878fb85
Luiz Muller Sales de Souza <mullerapns@gmail.com>
2025-11-19 14:11:04 -0400

    feat: Adding the projects finals with procedures and functions by value

--- 2nd-semester/programming-lab-ii/cpp-strings/caesar-cipher.cpp
+++ 2nd-semester/programming-lab-ii/cpp-strings/caesar-cipher.cpp
@@ -6,27 +6,27 @@
 // Constantes
 #define TAM 101
 
-// Importação de ferramentas
+// Importa??o de ferramentas
 using namespace std;
 
-// Função principal
+// Fun??o principal
 int main() {
 
-    // Função para idiomas
+    // Fun??o para idiomas
     setlocale(LC_ALL, "Portuguese");
 
-    // Variáveis
+    // Vari?veis
     int i = 0, j = 0;
     char phrase[TAM], alphabet[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    // Título
-    cout << "CIFRADOR DE CÓDIGO DE CÉSAR" << endl;
+    // T?tulo
+    cout << "CIFRADOR DE C?DIGO DE C?SAR" << endl;
 
     // Entrada e leitura de dados
-    cout << "\nDigite sua string (máx 100 caracteres): ";
+    cout << "\nDigite sua string (m?x 100 caracteres): ";
     gets(phrase);
 
-    // Convertendo a string para maiúsculo
+    // Convertendo a string para mai?sculo
     strupr(phrase);
 
     // Criptografia
@@ -38,10 +38,10 @@ int main() {
                 j++;
             }
 
-            // Aplicando o deslocamento de três letras
+            // Aplicando o deslocamento de tr?s letras
             j = (j + 3) % 26;
 
-            // Substituição do caractere cifrado
+            // Substitui??o do caractere cifrado
             phrase[i] = alphabet[j];
         }
     }
@@ -54,13 +54,13 @@ int main() {
 }
 
 /*
-    O código de César é uma das mais simples e conhecidas técnicas de
-    criptografia. É um tipo de substituição na qual cada letra do texto é
-    substituída por outra, que se apresenta no alfabeto abaixo dela um
-    número fixo de vezes. Por exemplo, com uma troca de três posições,
-    'A' seria substituído por 'D', 'B' se tornaria 'E', e assim por diante. Faça
-    um programa que implemente o uso desse Código de César (3 posições).
-    O programa deverá receber uma cadeia formada somente com caracteres
+    O c?digo de C?sar ? uma das mais simples e conhecidas t?cnicas de
+    criptografia. ? um tipo de substitui??o na qual cada letra do texto ?
+    substitu?da por outra, que se apresenta no alfabeto abaixo dela um
+    n?mero fixo de vezes. Por exemplo, com uma troca de tr?s posi??es,
+    'A' seria substitu?do por 'D', 'B' se tornaria 'E', e assim por diante. Fa?a
+    um programa que implemente o uso desse C?digo de C?sar (3 posi??es).
+    O programa dever? receber uma cadeia formada somente com caracteres
     do alfabeto e, em seguida, apresentar a cadeia codificada.
     Exemplo:
     Cadeia: a ligeira raposa marrom saltou sobre o cachorro cansado
