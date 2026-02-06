f5a571e46ca4f5ec6422e717775ac20c9cb937a7
Luiz Muller Sales de Souza <mullerapns@gmail.com>
2026-02-06 15:33:10 -0400

    docs: Adding comments for better code readability

--- 1st-semester/programming-lab-i/c-loops/c-for/power-calculation-loop.c
+++ 1st-semester/programming-lab-i/c-loops/c-for/power-calculation-loop.c
@@ -9,43 +9,39 @@ int main() {
     setlocale(LC_ALL, "Portuguese");
 
     // Variáveis
-    float base;
-    float result = 1.0; // Inicializado em 1 (elemento neutro da multiplicação)
-    int exponent;       // Expoente deve ser inteiro (Natural)
-    int i;
+    float base, result = 1.0;
+    int exponent, i;
 
     // Título
-    printf("CALCULADORA DE POTÊNCIA (VIA LOOP)\n\n");
+    printf("CALCULADORA DE POTÊNCIA EM LOOP FOR\n\n");
 
-    // Entrada da Base (Real)
+    // Entrada e leitura da base
     printf("Digite um valor real para a base: ");
     scanf("%f", &base);
 
-    // Entrada do Expoente (Natural/Inteiro)
+    // Entrada e leitura do expoente
     printf("Digite um valor natural para o expoente: ");
     scanf("%d", &exponent);
 
-    // Tratamento para expoentes negativos (opcional, mas boa prática)
+    // Lógica condicional para valores inválidos de expoente
     if (exponent < 0) {
-        printf("\nErro: Este programa só aceita expoentes naturais (>= 0).\n");
+        printf("\nErro: Este programa só aceita expoentes naturais (>= 0)\n");
         return 1;
     }
 
-    // Estrutura de repetição FOR
-    // Repete a multiplicação 'exponent' vezes
+    // Estrutura de repetição para calcular a potência
     for (i = 1; i <= exponent; i++) {
-        result = result * base; // ou result *= base;
+        result = result * base;
     }
 
     // Exibição do resultado
-    // %g remove zeros desnecessários
-    printf("\nResultado: %g elevado a %d = %g\n\n", base, exponent, result);
+    printf("\n%g ^ %d = %g\n", base, exponent, result);
 
     // Executado com sucesso
     return 0;
 }
 
 /*
-    Faça um programa que dados um número real x e um natural n, exiba a potência x^n.
-    Utilize a estrutura de repetição FOR.
+    Faça um programa que dados um número real x e um natural n,
+    exiba a potência x^n. Utilize a estrutura de repetição FOR.
 */
